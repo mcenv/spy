aa604b09003f4d384892f3c5f2f12e6c78213bb5
intsuc <intsuc@gmail.com>
2023-04-20 19:07:37 +0900

    Rename `Spy#execute` to `Spy#launch`

--- src/main/java/dev/mcenv/spy/Spy.java
+++ src/main/java/dev/mcenv/spy/Spy.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public final class Spy {
-  public static int execute(
-    final Class<?> register,
+  public static int launch(
+    final Class<?> commands,
     final String... args
   ) throws Throwable {
     final var java = ProcessHandle.current().info().command().orElseThrow();
     final var javaagent = Paths.get(Spy.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
-    final var type = register.getName().replace('.', '/');
+    final var type = commands.getName().replace('.', '/');
     final var classpath = System.getProperty("java.class.path");
     final var command = new ArrayList<String>();
     Collections.addAll(command, java, "-javaagent:" + javaagent + "=" + type, "-cp", classpath, Fork.class.getName());
