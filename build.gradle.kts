0e0585f9466f563fb79e43e721b4b47c655fbd37
intsuc <intsuc@gmail.com>
2023-04-20 11:59:03 +0900

    Add publish task and workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
   `java-library`
+  id("maven-publish")
 }
 
+group = "dev.mcenv"
 version = "0.1.0"
 
 repositories {
@@ -14,10 +16,32 @@ dependencies {
   implementation("org.ow2.asm:asm:9.5")
 }
 
+tasks.compileJava {
+  options.release.set(17)
+}
+
 tasks.jar {
   manifest {
     attributes(
-      "Premain-Class" to "spy.Agent",
+      "Premain-Class" to "dev.mcenv.spy.Agent",
     )
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHubPackages"
+      url = uri("https://maven.pkg.github.com/mcenv/spy")
+      credentials {
+        username = System.getenv("GITHUB_ACTOR")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+  publications {
+    register<MavenPublication>("gpr") {
+      from(components["java"])
+    }
   }
 }
