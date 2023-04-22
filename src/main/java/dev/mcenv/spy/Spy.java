a3a07874b956ecbe56333eae479c097c50ff69fb
intsuc <intsuc@gmail.com>
2023-04-22 16:47:03 +0900

    Support custom JVM arguments
    Closes #1.

--- src/main/java/dev/mcenv/spy/Spy.java
+++ src/main/java/dev/mcenv/spy/Spy.java
@@ -7,15 +7,24 @@ import java.util.Collections;
 public final class Spy {
   public static int launch(
     final Class<?> commands,
-    final String... args
+    final String[] jvmArgs,
+    final String[] mcArgs
   ) throws Throwable {
     final var java = ProcessHandle.current().info().command().orElseThrow();
     final var javaagent = Paths.get(Spy.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
     final var type = commands.getName().replace('.', '/');
     final var classpath = System.getProperty("java.class.path");
     final var command = new ArrayList<String>();
-    Collections.addAll(command, java, "-javaagent:" + javaagent + "=" + type, "-cp", classpath, Fork.class.getName());
-    Collections.addAll(command, args);
+    Collections.addAll(command, jvmArgs);
+    Collections.addAll(command, java, "-javaagent:" + javaagent + "=" + type, "-cp", '"' + classpath + '"', Fork.class.getName());
+    Collections.addAll(command, mcArgs);
     return new ProcessBuilder(command).inheritIO().start().waitFor();
+  }
+
+  public static int launch(
+    final Class<?> commands,
+    final String... mcArgs
+  ) throws Throwable {
+    return launch(commands, new String[0], mcArgs);
   }
 }
